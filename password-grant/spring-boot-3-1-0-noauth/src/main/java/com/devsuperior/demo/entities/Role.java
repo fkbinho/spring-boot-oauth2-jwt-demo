c0fce3b8fedce75550c1f370a55771907a723dc3
fkbinho <sir_kleber@hotmail.com>
2025-01-05 12:44:59 -0300

    Spring Security checklist, not tested

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/entities/Role.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/entities/Role.java
@@ -1,25 +1,18 @@
 package com.devsuperior.demo.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 
-import java.util.HashSet;
 import java.util.Objects;
-import java.util.Set;
 
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String authority;
-
-    @ManyToMany
-    @JoinTable(name = "tb_user_role",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles = new HashSet<>();
 
     public Role() {
     }
@@ -43,19 +36,6 @@ public class Role {
 
     public void setAuthority(String authority) {
         this.authority = authority;
-    }
-
-    public void addRole(Role role) {
-        roles.add(role);
-    }
-
-    public boolean hasRole(String roleName) {
-        for(Role role : roles) {
-            if (role.getAuthority().equals(roleName)) {
-                return true;
-            }
-        }
-        return false;
     }
 
     @Override
